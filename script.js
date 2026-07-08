88c0ce02e866a2cd33c69aba449f3839e0b2e46a
Nivek <condorinicolaskevin@gmail.com>
2026-07-08 14:33:03 -0400

    refactor: script.js and style.css

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const buttons = document.querySelectorAll("button");
 let firstNumber = "";
 let secondNumber = "";
 let operator = "";
+resultShow = false;
 
 for (let i = 0; i < buttons.length; i++) {
     const button = buttons[i];
@@ -13,8 +14,14 @@ for (let i = 0; i < buttons.length; i++) {
         const value = button.textContent;
 
         if (button.classList.contains("numbers")) {
+
+            if(resultShow){
+                screen.value = "";
+                resultShow = false;
+            }
+            
             screen.value += value
-            if(operator != ""){
+            if (operator != "") {
                 secondNumber += value;
             }
         }
@@ -29,30 +36,51 @@ for (let i = 0; i < buttons.length; i++) {
                 operator = "";
                 return;
             }
-            if(screen.value === ""){
+            if (screen.value === "") {
                 return;
             }
 
             firstNumber = screen.value;
             operator = value;
             screen.value += value;
-           
+
         }
 
 
         if (button.classList.contains("equals")) {
-            if (firstNumber === "" || operator === "" || secondNumber.value === "") {
+            if (firstNumber === "" || operator === "" || secondNumber === "") {
                 return;
             }
-            
+
             const numb1 = Number(firstNumber);
             const numb2 = Number(secondNumber);
-            let result
+            let result;
 
-            if(operator === "+"){
+            if (operator === "+") {
                 result = numb1 + numb2;
+
+            } else if (operator === "-") {
+                result = numb1 - numb2;
+
+            } else if (operator === "*") {
+                result = numb1 * numb2;
+
+            } else if (operator === "/" && numb2 !== 0) {
+                result = numb1 / numb2;
+
+            } else {
+                screen.value = "Error"
+                firstNumber = "";
+                secondNumber = "";
+                operator = "";
+                return;
             }
             screen.value = result;
+            firstNumber = "";
+            secondNumber = "";
+            operator = "";
+            resultShow = true;
+
         }
     });
 }
