6236fb1e1591a9438040fefdeb0e1fb940aec383
Nivek <condorinicolaskevin@gmail.com>
2026-07-08 17:39:59 -0400

    refactor: script.js

--- script.js
+++ script.js
@@ -4,83 +4,114 @@ const buttons = document.querySelectorAll("button");
 let firstNumber = "";
 let secondNumber = "";
 let operator = "";
-resultShow = false;
+let resultShow = false;
 
 for (let i = 0; i < buttons.length; i++) {
     const button = buttons[i];
 
     button.addEventListener("click", function () {
-
         const value = button.textContent;
 
         if (button.classList.contains("numbers")) {
-
-            if(resultShow){
-                screen.value = "";
-                resultShow = false;
-            }
-            
-            screen.value += value
-            if (operator != "") {
-                secondNumber += value;
-            }
-        }
-
-
-        if (button.classList.contains("operations")) {
-
-            if (value === "C") {
-                screen.value = "";
-                firstNumber = "";
-                secondNumber = "";
-                operator = "";
-                return;
-            }
-            if (screen.value === "") {
-                return;
-            }
-
-            firstNumber = screen.value;
-            operator = value;
-            screen.value += value;
-
-        }
-
-
-        if (button.classList.contains("equals")) {
-            if (firstNumber === "" || operator === "" || secondNumber === "") {
-                return;
-            }
-
-            const numb1 = Number(firstNumber);
-            const numb2 = Number(secondNumber);
-            let result;
-
-            if (operator === "+") {
-                result = numb1 + numb2;
-
-            } else if (operator === "-") {
-                result = numb1 - numb2;
-
-            } else if (operator === "*") {
-                result = numb1 * numb2;
-
-            } else if (operator === "/" && numb2 !== 0) {
-                result = numb1 / numb2;
-
-            } else {
-                screen.value = "Error"
-                firstNumber = "";
-                secondNumber = "";
-                operator = "";
-                return;
-            }
-            screen.value = result;
-            firstNumber = "";
-            secondNumber = "";
-            operator = "";
-            resultShow = true;
-
+            handleNumber(value);
+        } else if (button.classList.contains("operations")) {
+            handleOperation(value);
+        } else if (button.classList.contains("equals")) {
+            handleEquales();
         }
     });
 }
+
+
+function handleNumber(value) {
+    if (resultShow) {
+        screen.value = "";
+        resultShow = false;
+    }
+
+    screen.value += value
+
+    if (operator != "") {
+        secondNumber += value;
+    }
+}
+
+function clearCalculator() {
+        screen.value = "";
+        firstNumber = "";
+        secondNumber = "";
+        operator = "";
+}
+
+function handleOperation(value) {
+    if (value === "C") {
+        clearCalculator();
+        return;
+    }
+
+    if (screen.value === "") {
+        return;
+    }
+
+    if (operator !== "") {
+        return;
+    }
+
+    firstNumber = screen.value;
+    operator = value;
+    screen.value += value;
+    resultShow = false;
+}
+
+function calculate(numb1, numb2, operator) {
+    switch (operator){
+        case "+":
+            return numb1 + numb2;
+        case "-":
+            return numb1 - numb2;
+        case "*":
+            return numb1 * numb2;
+        case "/":
+            if(numb2 === 0){
+                return "Error";
+            }
+            return numb1 / numb2;
+        default:
+            return "Error";
+    }
+}
+
+function handleEquales(){
+    if (firstNumber === "" || operator === "" || secondNumber === "") {
+        return;
+    }
+
+    const num1 = Number(firstNumber);
+    const num2 = Number(secondNumber);
+
+    const result = calculate(num1, num2, operator);
+
+    screen.value = result;
+
+    firstNumber = "";
+    secondNumber = "";
+    operator = "";
+    resultShow = true;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
